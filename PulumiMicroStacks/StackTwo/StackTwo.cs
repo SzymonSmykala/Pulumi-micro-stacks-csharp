5e7c633e3921ddc88b15542b3cec24ff2b919c31
Szymon Smykała <ctk7000@gmail.com>
2022-08-06 19:44:14 +0200

    updated stack two to get output

---
+++ PulumiMicroStacks/StackTwo/StackTwo.cs
@@ -0,0 +1,23 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+
+class StackTwo : Stack
+{
+    public StackTwo()
+    {
+        var stackOne = new StackReference("devone");
+        var resourceGroupName = stackOne.GetOutput("ResourceGroupName");
+        
+        var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+        {
+            ResourceGroupName = resourceGroupName.Apply(x => x!.ToString())!,
+            Sku = new SkuArgs
+            {
+                Name = SkuName.Standard_LRS
+            },
+            Kind = Kind.StorageV2
+        });
+    }
+}
